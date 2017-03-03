64872684aff6b759abbf0f400e977f4511743f74
Jimmy Börjesson <alcesleo@gmail.com>
2017-03-03 16:05:14 +0100

    Add more columns to the database tables

--- db/migrations/20170303102347_create_watchers.rb
+++ db/migrations/20170303102347_create_watchers.rb
@@ -5,10 +5,14 @@ Hanami::Model.migration do
     create_table :watchers do
       uuid :id, primary_key: true, default: Sequel.function(:uuid_generate_v4)
 
-      column :url,        String,   null: false
-      column :selector,   String,   null: false
-      column :created_at, DateTime, null: false
-      column :updated_at, DateTime, null: false
+      column :url,         String,   null: false
+      column :selector,    String,   null: false
+      column :description, String,   null: false
+      column :email,       String,   null: false
+      column :notify,      :boolean, null: false, default: true
+      column :active,      :boolean, null: false, default: true
+      column :created_at,  DateTime, null: false
+      column :updated_at,  DateTime, null: false
     end
   end
 end
--- db/migrations/20170303104146_create_requests.rb
+++ db/migrations/20170303104146_create_requests.rb
@@ -3,7 +3,7 @@ Hanami::Model.migration do
     create_table :requests do
       uuid :id, primary_key: true, default: Sequel.function(:uuid_generate_v4)
 
-      foreign_key :watcher_id, :watchers, type: :uuid, null: false
+      foreign_key :watcher_id, :watchers, type: :uuid, null: false, on_delete: :cascade
 
       column :value,       String,   null: false
       column :status_code, Integer,  null: false
