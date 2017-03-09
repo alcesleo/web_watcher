674fd7878e69030e7972eb113e34fbecbe56e569
Jimmy Börjesson <alcesleo@gmail.com>
2017-03-09 14:50:18 +0100

    Test WatcherRepository

--- spec/web_watcher/repositories/watcher_repository_spec.rb
+++ spec/web_watcher/repositories/watcher_repository_spec.rb
@@ -1,5 +1,22 @@
 require "spec_helper"
 
 describe WatcherRepository do
-  # place your tests here
+  subject { WatcherRepository.new }
+  let(:email) { Faker::Internet.email }
+
+  before do
+    subject.clear
+
+    @watcher1 = Fabricate.create(:watcher, description: "ZZZ", email: email)
+    @watcher2 = Fabricate.create(:watcher, description: "AAA")
+    @watcher3 = Fabricate.create(:watcher, description: "BBB", active: false, email: email)
+  end
+
+  it "finds active watchers" do
+    subject.active_watchers.to_a.must_equal [@watcher1, @watcher2]
+  end
+
+  it "finds watchers by email" do
+    subject.find_by_email(email).to_a.must_equal [@watcher3, @watcher1]
+  end
 end
