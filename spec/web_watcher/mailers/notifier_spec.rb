c188ae4b4b5a90b415c12201d73182b9b60dbc19
Jimmy Börjesson <alcesleo@gmail.com>
2017-03-10 14:15:20 +0100

    Create Notifier mailer

---
+++ spec/web_watcher/mailers/notifier_spec.rb
@@ -0,0 +1,28 @@
+require "spec_helper"
+
+describe Mailers::Notifier do
+  let(:watcher)        { Fabricate.create(:watcher) }
+  let(:first_request)  { Fabricate.create(:request, watcher_id: watcher.id) }
+  let(:second_request) { Fabricate.create(:request, watcher_id: watcher.id) }
+  let(:from_email)     { "web_watcher_test@email.com" }
+
+  subject { Mailers::Notifier.deliver(watcher: watcher, request1: first_request, request2: second_request) }
+
+  before do
+    ENV.stubs(:fetch).with("FROM_EMAIL").returns(from_email)
+  end
+
+  it "delivers email" do
+    subject.to.must_equal      [watcher.email]
+    subject.subject.must_equal "Watcher '#{watcher.description}' has detected a change"
+
+    subject.body.encoded.must_include "Value changed from #{first_request.value} to #{second_request.value}"
+    subject.body.encoded.must_include "
+      The change took place between #{first_request.created_at} and #{second_request.created_at}".strip
+    subject.body.encoded.must_include Web.routes.watcher_path(watcher.id)
+  end
+
+  it "gets the from-addres from an ENV variable" do
+    subject.from.must_equal [from_email]
+  end
+end
