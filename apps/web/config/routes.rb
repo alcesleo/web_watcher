171d7e8e1dcaa21d0c6f80aa8e54570ad1ec3078
Jimmy Börjesson <alcesleo@gmail.com>
2018-02-23 23:33:40 +0100

    Add link to /users from /watchers

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -1,7 +1,7 @@
 # Configure your routes here
 # See: http://hanamirb.org/guides/routing/overview/
 
-get "/users/:email", to: "users#show"
+get "/users/:email", to: "users#show", as: :user
 
 resources :watchers, only: %i[new create show destroy] do
   member do
