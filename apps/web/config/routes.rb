43c47765faae34a60648a08d2545ba9d64df059c
Jimmy Börjesson <alcesleo@gmail.com>
2018-02-23 23:39:16 +0100

    Redirect root to new watcher

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -1,6 +1,8 @@
 # Configure your routes here
 # See: http://hanamirb.org/guides/routing/overview/
 
+redirect "/", to: "/watchers/new"
+
 get "/users/:email", to: "users#show", as: :user
 
 resources :watchers, only: %i[new create show destroy] do
